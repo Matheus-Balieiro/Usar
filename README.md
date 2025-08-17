cd779148ed476d06eeb194d7cb41dc6161602b9a
Matheus-Balieiro <mjfb@aluno.ifnmg.edu.br>
2025-08-17 23:04:02 +0000

    Teste

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Usar
+
+Para ti 
---
+++ main.js
@@ -0,0 +1,5 @@
+let particles = [];
+const numParticles = 4500;
+let baseHeartScale = 15;
+
+const loveText = "Voce e a pessoa mais bonita que ja apareceu na minha vida!"
